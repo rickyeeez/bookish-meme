41ca61465b51702ecf8fb6d638fc2682c1e999e3
rickyeeez <wardhanaricky30@gmail.com>
2024-06-29 21:57:11 +0700

    Add 50 Account

--- routes/routes.js
+++ routes/routes.js
@@ -42,7 +42,7 @@ router.get(
   "/UkIZhiRxJBwbX5AAquEsc1VElMCYfPiiEluxdZ9a1Di7QYjk3OO6fsH3Q0M5uapo/gigih",
   async (req, res) => {
     try {
-      const result = await Account.find().limit(150);
+      const result = await Account.find().limit(200);
 
       if (!result) {
         return res.status(404).json({ message: "Data not found" });
